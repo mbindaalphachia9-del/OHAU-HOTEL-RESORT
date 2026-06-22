95abe55f69e8b563b51529249d93b47dfd912f3b
mbindaalphachia9-del <mbindaalphachia9@gmail.com>
2026-06-22 23:31:47 +0100

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,21 +1,17 @@
 export default function Footer() {
   return (
-    <footer
-      style={{
-        marginTop: "100px",
-        padding: "40px",
-        textAlign: "center",
-        backgroundColor: "#111",
-        color: "white",
-      }}
-    >
+    <footer>
       <h2 style={{ color: "gold" }}>OAHU HOTEL & RESORT</h2>
+
+      <br />
 
       <p>Waikiki, Oahu, Hawaii</p>
 
       <p>📞 +1 (808) 555-2026</p>
 
       <p>📧 reservations@oahuhotelresort.com</p>
+
+      <br />
 
       <p>© 2026 OAHU HOTEL & RESORT. All rights reserved.</p>
     </footer>
